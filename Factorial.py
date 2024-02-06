2b46d0f28a46c1b393f42d90fd724226e8e4e3d7
akhmerov1989 <sergeyakhmerov@gmail.com>
2024-02-06 13:36:40 -0500

    Added recursion method to Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -3,8 +3,28 @@ factorial = 1
 if num < 0:
     print("Factorial does not exist for negative numbers")
 elif num == 0:
-    print("Factorial of ) is 1")
+    print("Factorial of 0 is 1")
 else:
     for i in range(1, num + 1):
         factorial = factorial * i
     print(f'The factorial of {num} is {factorial}')
+
+
+# Find factorial using recursion
+
+def recur_factorial(n):
+    if n == 1:
+        return n
+    else:
+        return n * recur_factorial(n - 1)
+
+
+num = int(input("Enter the number: "))
+
+# Check if the number is negative
+if num < 0:
+    print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+    print("The factorial of 0 is 1")
+else:
+    print("The factorial of", num, "is", recur_factorial(num))
