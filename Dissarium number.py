5bd25ab82efa56d4f6b232f0a4f0a2635575eacc
akhmerov1989 <sergeyakhmerov@gmail.com>
2024-02-09 09:00:18 -0500

    Added a Disarium number.py

---
+++ Dissarium number.py
@@ -0,0 +1,23 @@
+""" Disarium number is a number that is equal to the sum of its digits each raised to the power of its
+ respective position. ( 89 = 8**1 + 9 **2 = 8 + 81)"""
+
+def is_disarium(number):
+    # Convert the number to a string to iterate over its digits
+    num_str = str(number)
+
+    # Calculation the sum
+    digit_sum = sum(int(i)**(index + 1) for index, i in enumerate(num_str))
+
+    # Check if the sum is equal to the original
+    return digit_sum == number
+# Input a number
+try:
+    num = int(input("Enter a number: "))
+
+    # Check
+    if is_disarium(num):
+        print(f"{num} is a Disarium number.")
+    else:
+        print(f"{num} is not Disarium number.")
+except ValueError:
+    print("Invalid input. Please enter a valid numnber.")
