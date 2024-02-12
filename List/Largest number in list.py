1bb81b64b6754372dee72a943d678c71e69a8631
akhmerov1989 <sergeyakhmerov@gmail.com>
2024-02-12 10:57:17 -0500

    Added function to Largest number in list.py

--- List/Largest number in list.py
+++ List/Largest number in list.py
@@ -17,3 +17,20 @@ if len(numbers) >= 2:
     print("The second largest number in the list is: ", second_largest)
 else:
     print("The list does not contain a second largest number.")
+
+
+# N largest elements in the list
+def find_n_largest_elements(lst, n):
+    sorted_lst = sorted(lst, reverse=True)
+    # Get the first N elements
+    largest_elements = sorted_lst[:n]
+    return largest_elements
+
+
+numbers = [30, 10, 340, 56, 76, 7, 8, 4, 6, 5, 65, 708]
+
+N = int(input("N = "))
+result = find_n_largest_elements(numbers, N)
+print(f"THe {N} largest elements in the list are: ", result)
+
+
