ace35fa9b75c44b4ab211ccca0711ff2f7d67200
akhmerov1989 <sergeyakhmerov@gmail.com>
2024-02-16 11:17:51 -0500

    Added Sum of all items

---
+++ Operations with dictionaries/Sum of all items.py
@@ -0,0 +1,13 @@
+my_dict = {
+    'a': 10,
+    'b': 20,
+    'c': 30,
+    'd': 40,
+    'e': 50
+}
+
+total_sum = 0
+for i in my_dict.values():
+    total_sum += i
+
+print("Sum of all items: ", total_sum)
